2a68c3597940a7809305cb52b9975fce5343ea17
Achiratch <48543080+Achiratch@users.noreply.github.com>
2019-05-24 01:11:04 +0700

    Add files via upload

---
+++ ADDFIT/JS/link.js
@@ -0,0 +1,14 @@
+
+
+function relocate_register() {
+    location.href = "register.html";
+} 
+
+function relocate_login() {
+    location.href = "login.html";
+}
+
+function relocate_welcome() {
+    location.href = "welcome.html";
+}
+
