0424eae7a5c99a9f754e67f922bd576628778a5b
Achiratch <48543080+Achiratch@users.noreply.github.com>
2019-05-27 01:58:20 +0700

    Add files via upload

---
+++ Web-pro-555/public/JS/link.js
@@ -0,0 +1,25 @@
+
+
+function relocate_register() {
+    location.href = "register.html";
+} 
+
+function relocate_login() {
+    location.href = "login.html";
+}
+
+function relocate_welcome() {
+    location.href = "welcome.html";
+}
+
+function relocate_profile() {
+    location.href = "profile.html"
+}
+
+function relocate_BMI() {
+    location.href = "BMI.html"
+}
+
+function relocate_fitness() {
+    location.href = "fitness.html" 
+}
